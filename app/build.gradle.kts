0b12ad959f2d156f586918ef504200113627e3e6
Hendry Agus Setiawan <hue.huendry@gmail.com>
2026-06-16 21:46:12 +0700

    chore: update target sdk to 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,12 +9,12 @@ plugins {
 
 android {
     namespace = "com.hendry.saku"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.hendry.saku"
         minSdk = 32
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
